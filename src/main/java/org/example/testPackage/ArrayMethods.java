13d21609d13363bb2d7e6c49f56f424ec3072ddf
1034517 <shashank.kumar@blueyonder.com>
2022-10-11 10:37:32 +0530

    basic assertions in Junit

---
+++ src/main/java/org/example/testPackage/ArrayMethods.java
@@ -0,0 +1,26 @@
+package org.example.testPackage;
+
+public class ArrayMethods {
+
+    public int findIndex(int[] arr, int num){
+
+        int index=-1;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]== num){
+                index =i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public boolean searchNumber(int[] arr, int num){
+        boolean found = false;
+
+        for(Integer i:arr){
+            if(i==num) found =true;
+        }
+        return found;
+    }
+}
---
+++ src/test/java/ArrayTesting.java
@@ -0,0 +1,64 @@
+import org.example.testPackage.ArrayMethods;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayTesting {
+
+    @Test
+    public void finIndexTest(){
+
+        ArrayMethods obj = new ArrayMethods();
+
+        int result = obj.findIndex(new int[]{1,2,3,4,5,6}, 4);
+
+        assertEquals(3,result);
+    }
+
+    @Test
+    public void searchNumberTest(){
+
+        ArrayMethods obj = new ArrayMethods();
+
+        boolean result = obj.searchNumber(new int[]{1,2,3,4,5,6}, 4);
+
+        System.out.println(result);
+
+        assertEquals(true,result);
+
+        assertTrue(result);
+
+        String str =null;
+        assertNull(str);
+        assertNotNull(result);
+       // assertEquals(null, result);
+
+        fail("Unfinished method");
+
+
+
+    }
+
+    @Test
+    public void findIndexEmpty(){
+
+        ArrayMethods obj = new ArrayMethods();
+
+        assertEquals(-1,obj.findIndex(new int[]{}, 2));
+
+    }
+
+    //Test Asserts
+    @Test
+    public void testAsserts(){
+
+         boolean result = true;
+         assertEquals(true, result);
+         assertTrue(result);
+
+    }
+
+
+
+}
